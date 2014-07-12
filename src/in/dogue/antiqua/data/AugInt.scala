94f697ec4d902d51bfb1e3328c28e1506a1faf12
olive <olivine.dogue@gmail.com>
2014-07-12 13:06:45 -0400

    add safe subtration methods

--- src/in/dogue/antiqua/data/AugInt.scala
+++ src/in/dogue/antiqua/data/AugInt.scala
@@ -1,5 +1,20 @@
 package in.dogue.antiqua.data
 
+import in.dogue.antiqua.Implicits
+import Implicits._
+
 class AugInt(a:Int) {
-    def %%(b:Int) = (a % b + b) % b
+  def %%(b:Int) = (a % b + b) % b
+  def clamp(min:Int, max:Int) = {
+    if (a < min) {
+      min
+    } else if (a > max) {
+      max
+    } else {
+      a
+    }
+  }
+
+  def drop1 = (a - 1).clamp(0, Int.MaxValue)
+  def drop(i:Int) = (a - i).clamp(0, Int.MaxValue)
 }
