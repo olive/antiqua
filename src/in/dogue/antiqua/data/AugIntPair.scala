e2d62bc7b3bd037e725cbde54c1de66460fe0956
olive <olivine.dogue@gmail.com>
2014-07-13 13:00:49 -0400

    added utility methods

--- src/in/dogue/antiqua/data/AugIntPair.scala
+++ src/in/dogue/antiqua/data/AugIntPair.scala
@@ -3,6 +3,13 @@ package in.dogue.antiqua.data
 class AugIntPair(p:(Int,Int)) {
   def x = p._1
   def y = p._2
-  def +(q:(Int,Int)) = (p._1 + q._1, p._2 + q._2)
+  def |+|(pr:(Int,Int)) = (p._1 + pr._1, p._2 + pr._2)
+  def |+(i:Int) = (p._1 + i, p._2)
+  def +|(j:Int) = (p._1, p._2 + j)
+
+  def |-|(pr:(Int,Int)) = (p._1 - pr._1, p._2 - pr._2)
+  def |-(i:Int) = (p._1 - i, p._2)
+  def -|(j:Int) = (p._1, p._2 - j)
+
   def -->(d:Direction) = (p._1 + d.dx, p._2 + d.dy)
 }
