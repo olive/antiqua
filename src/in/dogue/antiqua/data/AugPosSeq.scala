b981e0b706fe5514568383e1c9f0d4bec3897f7c
olive <olivine.dogue@gmail.com>
2014-07-13 11:07:19 -0400

    added utility functions

--- src/in/dogue/antiqua/data/AugPosSeq.scala
+++ src/in/dogue/antiqua/data/AugPosSeq.scala
@@ -3,4 +3,6 @@ package in.dogue.antiqua.data
 class AugPosSeq[T](s:Seq[(Int,Int,T)]) {
   def smap(f:T => T) = s.map{ case (i, j, t) => (i, j, f(t))}
   def |+|(p:Int, q:Int) = s.map { case (i, j, t) => (i + p, j + q, t)}
+  def +|(q:Int) = s.map { case (i, j, t) => (i, j + q, t)}
+  def |+(p:Int) = s.map { case (i, j, t) => (i + p, j, t)}
 }
