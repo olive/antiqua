57e8ee646121df621bcef1ebed67868a001536c5
olive <olivine.dogue@gmail.com>
2014-07-18 10:33:03 -0400

    add magnitude function

--- src/in/dogue/antiqua/data/AugIntPair.scala
+++ src/in/dogue/antiqua/data/AugIntPair.scala
@@ -1,5 +1,8 @@
 package in.dogue.antiqua.data
 
+import in.dogue.antiqua.Antiqua
+import Antiqua._
+
 class AugIntPair(p:(Int,Int)) {
   @inline def x = p._1
   @inline def y = p._2
@@ -10,6 +13,8 @@ class AugIntPair(p:(Int,Int)) {
   def |-|(pr:(Int,Int)) = (p._1 - pr._1, p._2 - pr._2)
   def |-(i:Int) = (p._1 - i, p._2)
   def -|(j:Int) = (p._1, p._2 - j)
+
+  @inline def mag = (x*x + y*y).sqrt
 
   def -->(d:Direction) = (p._1 + d.dx, p._2 + d.dy)
 }
