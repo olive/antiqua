dec867360681ebb7a7bbb6a26b19656d0cf19cc7
olive <olivine.dogue@gmail.com>
2014-07-23 13:15:28 -0400

    cons method for tuples

--- src/in/dogue/antiqua/data/AugTuple2.scala
+++ src/in/dogue/antiqua/data/AugTuple2.scala
@@ -3,4 +3,5 @@ package in.dogue.antiqua.data
 class AugTuple2[A,B](t:(A,B)) {
   def map1[K](f:A => K) = t.copy(_1=f(t._1))
   def map2[K](f:B => K) = t.copy(_2=f(t._2))
+  def ++[C](c:C) = (t._1, t._2, c)
 }
--- src/in/dogue/antiqua/data/AugTuple4.scala
+++ src/in/dogue/antiqua/data/AugTuple4.scala
@@ -5,4 +5,5 @@ class AugTuple4[A,B,C,D](t:(A, B, C, D)) {
   def map2[K](f:B => K) = t.copy(_2=f(t._2))
   def map3[K](f:C => K) = t.copy(_3=f(t._3))
   def map4[K](f:D => K) = t.copy(_4=f(t._4))
+  def ++[E](c:E) = (t._1, t._2, t._3, t._4, c)
 }
