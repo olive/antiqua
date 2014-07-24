1d397ed69b5ecd5954934b5b5b2bd4ddeb817ac1
olive <olivine.dogue@gmail.com>
2014-07-24 14:39:23 -0400

    remove unneeded call

--- src/in/dogue/antiqua/utils/TmxMap.scala
+++ src/in/dogue/antiqua/utils/TmxMap.scala
@@ -10,4 +10,9 @@ class TmxMap(name:String, layer:String) {
   val rows = tiles.length
   val cols = tiles(0).length
   def get(ij:Cell) = tiles(ij.y)(ij.x) //[sic]
+  def getOption(ij:Cell) = {
+    val x = ij.x
+    val y = ij.y
+    get(ij).onlyIf(x > 0 && x <= cols - 1 && y > 0 && y <= rows - 1)
+  }
 }
