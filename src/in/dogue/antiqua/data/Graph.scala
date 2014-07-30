cbfe888ddd6c05073ba49ca13158d65c3b00d672
olive <olivine.dogue@gmail.com>
2014-07-30 15:16:19 -0400

    added graph class and flood fill

---
+++ src/in/dogue/antiqua/data/Graph.scala
@@ -0,0 +1,47 @@
+package in.dogue.antiqua.data
+
+import scala.annotation.tailrec
+import in.dogue.antiqua.Antiqua
+import Antiqua._
+
+object Graph {
+  def flood[C,N](g:Graph[C,N], seed:C):Set[C] = {
+    var set = Set[C]() + seed
+    var filled = Set[C]()
+    while (!set.isEmpty) {
+      val Some((pos, newSet)) = set.takeAny
+      filled = filled + pos
+      set = newSet
+      val ns = g.getNeighbors(pos)
+      ns foreach { case ppos =>
+        if (!filled.contains(ppos)) {
+          set = set + ppos
+        }
+
+      }
+    }
+    filled
+  }
+
+  def floodAll[C, N](g:Graph[C, N], all:Set[C]) = {
+    floodAllHelper(g, all, Seq())
+
+  }
+
+  @tailrec
+  final def floodAllHelper[C, N](g:Graph[C, N], open:Set[C], sofar:Seq[Set[C]]):Seq[Set[C]] = {
+    open.takeAny match {
+      case Some((seed, rest)) =>
+        val filled = flood(g, seed)
+        val newOpen = open -- filled
+        floodAllHelper(g, newOpen, filled +: sofar)
+      case None => sofar
+    }
+  }
+}
+
+trait Graph[TCoord, TNode] {
+  def getNeighbors(c:TCoord):Seq[TCoord]
+  def get(c:TCoord):TNode
+}
+
