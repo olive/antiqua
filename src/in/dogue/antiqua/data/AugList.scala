9424da8b4b98ee0a83ae7d31415100391799a64c
olive <olivine.dogue@gmail.com>
2014-08-10 14:27:19 -0400

    add fold functions

--- src/in/dogue/antiqua/data/AugList.scala
+++ src/in/dogue/antiqua/data/AugList.scala
@@ -17,4 +17,20 @@ class AugList[T](t:List[T]) {
     inner(List(), t)
 
   }
+
+
+
+  def fold2[B](b:B, f:(T, B) => (T, B)):(List[T], B) = {
+    t.foldLeft((List[T](), b)) { case ((acc, w), x) =>
+      val (p, q) = f(x,w)
+      (p :: acc) @@ q
+    }
+  }
+
+  def fold3[B,C](b:B, c:C, f:(T, B, C) => (T, B, C)):(List[T], B, C) = {
+    t.foldLeft((List[T](), b, c)) { case ((acc, w, y), x) =>
+      val (p, q, r) = f(x,w,y)
+      (p :: acc) @@ q @@ r
+    }
+  }
 }
--- src/in/dogue/antiqua/data/AugSeq.scala
+++ src/in/dogue/antiqua/data/AugSeq.scala
@@ -16,5 +16,4 @@ class AugSeq[T](s:Seq[T]) {
     }
   }
 
-
 }
