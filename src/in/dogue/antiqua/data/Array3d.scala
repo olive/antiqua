b8e0f835a197091512d50e2b6c7a7f8587ec9cb4
olive <olivine.dogue@gmail.com>
2014-08-11 16:26:56 -0400

    added array3d

---
+++ src/in/dogue/antiqua/data/Array3d.scala
@@ -0,0 +1,34 @@
+package in.dogue.antiqua.data
+
+import in.dogue.antiqua.Antiqua
+import Antiqua._
+
+object Array3d {
+  def tabulate[T](cols:Int, rows:Int, layers:Int)(f:Vox => T) = {
+    val vs = Vector.tabulate(layers) { k =>
+      Array2d.tabulate(cols, rows) { case (i, j) => f((i, j, k))}
+    }
+    Array3d(cols, rows, layers, vs)
+  }
+}
+
+case class Array3d[T](cols:Int, rows:Int, layers:Int, vs:Vector[Array2d[T]]) {
+  @inline private def cpy(v:Vector[Array2d[T]]) = Array3d(cols, rows, layers, v)
+  def get(v:Vox):T = vs(v.z).get(v.xy)
+  def getOption(v:Vox):Option[T] = vs(v.z).getOption(v.xy) //fixme, not safe in z index
+  def updated(v:Vox, t:T):Array3d[T] = cpy(vs.updated(v.z, vs(v.z).updated(v.xy, t)))
+  def update(v:Vox, f:T=>T):Array3d[T]= cpy(vs.updated(v.z, vs(v.z).update(v.xy, f)))
+  def flatten:Seq[(Vox,T)] = vs.zipWithIndex.map{ case (a, z) => a.flatten.map { case ((i, j), t) => ((i, j, z), t)}}.flatten
+  def toGraph:Graph[Vox,Vox] = new Graph[Vox,Vox] {
+    def get(v:Vox) = v
+    def getNeighbors(v:Vox):Seq[Vox] = for {
+      i <- -1 to 1
+      j <- -1 to 1
+      k <- -1 to 1
+      if i != 0 || j != 0 || k != 0
+    } yield {
+      (i, j, k) |+| v
+    }
+
+  }
+}
